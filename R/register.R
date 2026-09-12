1794242c3faf6b16916cefd9f760be1b67e2a471
Kirill Müller <kirill@cynkra.com>
2026-09-12 16:47:33 +0000

    feat: Support packages with multiple dots in the name

--- R/register.R
+++ R/register.R
@@ -61,7 +61,7 @@ cpp_register <- function(
   funs <- get_registered_functions(all_decorations, "cpp11::register", quiet)
 
   package <- desc::desc_get("Package", file = file.path(path, "DESCRIPTION"))
-  package <- sub("[.]", "_", package)
+  package <- gsub("[.]", "_", package)
 
   cpp_functions_definitions <- generate_cpp_functions(funs, package)
 
